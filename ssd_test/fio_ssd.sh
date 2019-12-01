dd28bc87ced25b2e845d2e3f5b1169748fc8470d
K <1339764596@qq.com>
2019-12-01 16:01:57 +0800

    test

--- ssd_test/fio_ssd.sh
+++ ssd_test/fio_ssd.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-threads=("1" "2" "4"  "6" "8" "10" "12" "14" "16" "24" "48")
+threads=("1" "2" "4" "6" "8" "10" "12" "14" "16")
 threads_s=0
 threads_len=9
 nr_files=1
 all_size=322122547200
 
 cpus=('0-9,20-29' '0-9,20-29'  '0-9,20-29'  '0-9,20-29' '0-9,20-29')
-name=('a'  'a' 'a'  'a' '01' 'a' '0111' '0110' '0001' )
-files_s=3
-files_len=4
 
-bs_size=("8B" "16B" "32B" "64B" "256B" "1KB" "4KB" "16KB" "64KB" "256KB")
+bs_size=("8B" "16B" "32B" "64B" "128B" "256B" "512B" "1KB" "4KB" "16KB" "64KB" "256KB" "1MB" "4MB" "16MB")
 size_s=0
-size_len=10
+size_len=15
 
 type=('read' 'write' 'randread' 'randwrite')
 type_s=0
@@ -26,19 +23,13 @@ work_path=`pwd`
 test_times=1
 branch="-b 1"
 depth="-z 1"
-file_name="nova_multi"
-run_avg="--allow-run-as-root"
-run_avg=""
-host="--hostfile hostfile"
-host=""
-iterates=10
 dev_name=/dev/nvme0n1
-fallocate="--fallocate=0"
-runtime=60
+fallocate="--fallocate=1"
+runtime=30
 
 rm -rf *.results
-# pre_size=397284474880
-pre_size=$((1024*1024))
+pre_size=397284474880
+# pre_size=$((1024*1024))
 fio --filename=$dev_name -rw=trim -ioengine=libaio --direct=1 -bs=128KB -name=test -iodepth=1 -size=$pre_size --nrfiles=1 --numjobs=1 
 fio --filename=$dev_name -rw=write -ioengine=libaio --direct=1 -bs=128KB -name=test -iodepth=1 -size=$pre_size --nrfiles=1 --numjobs=1 
 fio --filename=$dev_name -rw=randwrite -ioengine=libaio --direct=1 -bs=4KB -name=test -iodepth=1 -size=$pre_size  --nrfiles=1  --numjobs=1 
@@ -54,8 +45,7 @@ for((io=$io_s; io<$io_len; ++io)); # libio
             do
                 for((times=0; times<$test_times; ++times)); 
                 do
-                echo $file_size
-                fio --filename=$dev_name -rw=${type[$t]} -ioengine=${ioengine[$io]} -runtime=$runtime -time_based --direct=1 -bs=${bs_size[$size]} -name=test -iodepth=1 -size=$all_size $fallocate  --nrfiles=${nr_files} --cpus_allowed=${cpus[$f]} --numjobs=${threads[$i]} > ${times}_${type[$t]}_${ioengine[$io]}_${bs_size[$size]}_${threads[$i]}_${name[$f]}.results
+                fio --filename=$dev_name -rw=${type[$t]} -ioengine=${ioengine[$io]} -runtime=$runtime -time_based --direct=1 -bs=${bs_size[$size]} -name=test -iodepth=1 -size=$all_size $fallocate  --nrfiles=${nr_files} --cpus_allowed=${cpus[$f]} --numjobs=${threads[$i]} > ${type[$t]}_${ioengine[$io]}_${bs_size[$size]}_${threads[$i]}.results
                 done
             done
         done

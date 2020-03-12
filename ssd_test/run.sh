c226a8d570629f873891a9c2cd2c570b0b6af872
K <1339764596@qq.com>
2020-03-12 13:29:40 +0800

    test

--- ssd_test/run.sh
+++ ssd_test/run.sh
@@ -1,14 +1,16 @@
-BLOCK_SIZE=$((1*4096))
+block_name=(07_512B 08_1KB 09_4KB 10_16KB 11_64KB 12_256KB 13_1MB 14_4MB 15_16MB)
+block_size=(512 1024 4096 16384 65536 262144 1048576 4194304 16777216)
 TOTAL_SIZE=$((16*1024))
+RESULT_PATH=result
+mkdir $RESULT_PATH
 
-rm -rf /home/hanshukai/optane/*.io
-
-for ((i=1; i<=2; i++))
+for ((i=0; i<${#block_size[*]}; i+=1))
 do
+rm -rf /home/hanshukai/optane/*.io
 for ((j=1; j<=1; j++))
 do
 # perf record ./tester $i 1 $BLOCK_SIZE $TOTAL_SIZE
-./tester $i 2 1 $BLOCK_SIZE $TOTAL_SIZE
+./tester $i 2 1 ${block_size[$i]} $TOTAL_SIZE > $RESULT_PATH/${block_name[$i]}
 # mv perf.data $RESULT_PATH/$i_$j
 sync && echo 3 > /proc/sys/vm/drop_caches
 done

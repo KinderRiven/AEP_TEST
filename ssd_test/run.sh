242ace7148579aa6c79e2e2d221277e4af5243b8
K <1339764596@qq.com>
2020-03-12 13:15:57 +0800

    test

--- ssd_test/run.sh
+++ ssd_test/run.sh
@@ -1,15 +1,14 @@
-RESULT_PATH=dd_result
 BLOCK_SIZE=$((1*4096))
-TOTAL_SIZE=$((4*1024))
+TOTAL_SIZE=$((16*1024))
 
 mkdir $RESULT_PATH
 for ((i=1; i<=3; i++))
 do
 rm -rf 0.io
-for ((j=1; j<=10; j++))
+for ((j=1; j<=1; j++))
 do
 # perf record ./tester $i 1 $BLOCK_SIZE $TOTAL_SIZE
-./tester $i 1 $BLOCK_SIZE $TOTAL_SIZE
+./tester $i 2 1 $BLOCK_SIZE $TOTAL_SIZE
 # mv perf.data $RESULT_PATH/$i_$j
 sync && echo 3 > /proc/sys/vm/drop_caches
 done
